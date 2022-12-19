552af9ecd0b4dac8e45ae6f5265e931dbb116591
popovic-nedeljko <nftnedja@gmail.com>
2022-12-19 23:36:01 +0100

    Increase and decrease buttonm functionality

--- src/reducer.js
+++ src/reducer.js
@@ -9,6 +9,33 @@ const reducer = (state, action) => {
       cart: state.cart.filter((item) => item.id !== action.payload),
     };
   }
+  if (action.type === `INCREASE`) {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return {
+      ...state,
+      cart: tempCart,
+    };
+  }
+
+  if (action.type === `DECREASE`) {
+    let tempCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, amount: item.amount - 1 };
+        }
+        return item;
+      })
+      .filter((item) => item.amount !== 0);
+    return {
+      ...state,
+      cart: tempCart,
+    };
+  }
 
   return state;
 };
